3f142940f638e46e9d5682815103479075048bce
Michal Diviš <michall.divis@gmail.com>
2022-09-07 11:54:10 +0200

    added build project

---
+++ build/Build.cs
@@ -0,0 +1,105 @@
+using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Utilities.Collections;
+using System.Linq;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+[ShutdownDotNetAfterServerBuild]
+class Build : NukeBuild
+{
+    public static int Main() => Execute<Build>(x => x.PushNuget);
+
+    private const string CoreProjectName = "DarkRandomUtils";
+    private const string TestProjectName = "DarkRandomUtils.Tests";
+
+    [Solution] readonly Solution Solution;
+
+    AbsolutePath SourceDirectory => RootDirectory / "src";
+    AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+
+    readonly Configuration Configuration = Configuration.Release;
+
+    [Parameter] readonly string NugetApiUrl = "https://api.nuget.org/v3/index.json";
+    [Parameter] readonly string NugetApiKey;
+
+    Target Clean => _ => _
+        .Before(RestoreCore)
+        .Executes(() =>
+        {
+            SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+        });
+
+    Target RestoreCore => _ => _
+        .Executes(() =>
+        {
+            DotNetRestore(s => s
+                .SetProjectFile(Solution.GetProject(CoreProjectName)));
+        });
+
+    Target CompileCore => _ => _
+        .DependsOn(RestoreCore)
+        .Executes(() =>
+        {
+            DotNetBuild(s => s
+                .SetProjectFile(Solution.GetProject(CoreProjectName))
+                .SetConfiguration(Configuration)
+                .EnableNoRestore());
+        });
+
+    Target CompileTests => _ => _
+        .DependsOn(CompileCore)
+        .Executes(() =>
+        {
+            DotNetBuild(s => s
+                .SetProjectFile(Solution.GetProject(TestProjectName))
+                .SetConfiguration(Configuration)
+                .EnableNoRestore());
+        });
+
+    Target RunTests => _ => _
+        .DependsOn(CompileTests)
+        .Executes(() =>
+        {
+            DotNetTest(s => s
+                .SetProjectFile(Solution.GetProject(TestProjectName))
+                .SetConfiguration(Configuration)
+                .EnableNoRestore()
+                .EnableNoBuild());
+        });
+
+
+    Target PackCore => _ => _
+        .DependsOn(RunTests)
+        .Executes(() =>
+        {
+            DotNetPack(s => s
+              .SetProject(Solution.GetProject(CoreProjectName))
+              .SetConfiguration(Configuration)
+              .EnableNoBuild()
+              .EnableNoRestore()
+              .SetOutputDirectory(ArtifactsDirectory));
+        });
+
+    Target PushNuget => _ => _
+        .DependsOn(PackCore)
+        .Requires(() => NugetApiUrl)
+        .Requires(() => NugetApiKey)
+        .Executes(() =>
+        {
+            GlobFiles(ArtifactsDirectory, "*.nupkg")
+               .Where(x => !string.IsNullOrEmpty(x) && !x.EndsWith("symbols.nupkg"))
+               .ForEach(x =>
+               {
+                   DotNetNuGetPush(s => s
+                       .SetTargetPath(x)
+                       .SetSource(NugetApiUrl)
+                       .SetApiKey(NugetApiKey)
+                   );
+               });
+        });
+}
